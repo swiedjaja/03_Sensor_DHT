762b584c4fe2a75fba7047a5d8d5603b62582878
swiedjaja <swiedjaja@gmail.com>
2022-05-12 02:23:44 +0700

    add esp32-c3

--- include/device.h
+++ include/device.h
@@ -4,7 +4,9 @@
   #define LED_GREEN  D6
   #define PIN_SW     D5
   #define PIN_DHT    D3
-#elif defined(ESP32)
+  #define LED_BUILTIN_ON  0
+  #define LED_BUILTIN_OFF 1
+#elif defined(ESP32_CAM)
   #if !defined(LED_BUILTIN) // ESP32Cam
     #define LED_BUILTIN 33
   #endif
@@ -14,10 +16,26 @@
   #define LED_GREEN  16
   #define PIN_SW     0
   #define PIN_DHT    13 // pin 2, 12: program failed, 16: dht read error
+  #define LED_BUILTIN_ON  0
+  #define LED_BUILTIN_OFF 1
+#elif defined(NODEMCU_ESP32_C3_13)
+  #define LED_BUILTIN_RED GPIO_NUM_3
+  #define LED_BUILTIN_GREEN GPIO_NUM_4
+  #define LED_BUILTIN_BLUE GPIO_NUM_5
+  #define LED_BUILTIN_COOL GPIO_NUM_19
+  #define LED_BUILTIN_WARM GPIO_NUM_18
+  #define LED_BUILTIN LED_BUILTIN_WARM
+  #define LED_RED     LED_BUILTIN_RED
+  #define LED_YELLOW  LED_BUILTIN_WARM
+  #define LED_GREEN   LED_BUILTIN_GREEN
+  #define PIN_SW     6
+  // #define PIN_SDA    1
+  // #define PIN_SCL    2
+  #define PIN_DHT    2 
+  #define LED_BUILTIN_ON  1
+  #define LED_BUILTIN_OFF 0
 #endif
 
-#define LED_BUILTIN_ON  0
-#define LED_BUILTIN_OFF 1
 
 #define LED_ON  1
 #define LED_OFF 0
